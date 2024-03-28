f32fe02b9ab3319039def6ce4d333cecfe3f0b1c
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-03-28 17:09:58 -0400

    project modificado

--- src/pages/project/project.jsx
+++ src/pages/project/project.jsx
@@ -1,7 +1,16 @@
 export const MyProject = () => {
   return (
     <div>
-      <h1>My Project</h1>
+      <h1>Meus Projetos</h1>
+      <div>
+        <h2>Projeto 1</h2>
+      </div>
+      <div>
+        <h2>Projeto 2</h2>
+      </div>
+      <div>
+        <h2>Projeto 3</h2>
+      </div>
     </div>
   );
 };
